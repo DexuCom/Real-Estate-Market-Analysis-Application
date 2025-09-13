0ddf148a8ac22398ca38d491ebe9c3640126b045
Pablo1618 <pawelwawrzynski22@gmail.com>
2025-09-13 18:00:23 +0200

    Removed localhost:5500 from setAllowedOrigins list

--- Backend/gateway-app/src/main/java/com/rema/gateway_app/CorsConfig.java
+++ Backend/gateway-app/src/main/java/com/rema/gateway_app/CorsConfig.java
@@ -17,8 +17,7 @@ public class CorsConfig {
         final CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(Arrays.asList(
-            "http://127.0.0.1:5500",
-            "http://localhost:5500"
+            "http://127.0.0.1:5500"
         ));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
